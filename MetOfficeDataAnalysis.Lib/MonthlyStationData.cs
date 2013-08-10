2c1feb545160fe6e122395065988103ff3e07b74
Robert Impey <robert@impey.info>
2013-08-10 16:47:36 +0200

    Able to parse the weather station text files.

--- MetOfficeDataAnalysis.Lib/MonthlyStationData.cs
+++ MetOfficeDataAnalysis.Lib/MonthlyStationData.cs
@@ -17,7 +17,7 @@ namespace MetOfficeDataAnalysis.Lib
         public bool? CampbellStokes { get; private set; } // Sunshine hours measured with Kipp & Zonen or Campbell Stokes device.
         public bool Provisional { get; private set; }
 
-        public MonthlyStationData(int year, int month, double? maxTemperature, double? minTemperature, 
+        public MonthlyStationData(int year, int month, double? maxTemperature, double? minTemperature,
             int? airFrost, double? rain, double? sunshine, bool? campbellStokes, bool provisional)
         {
             Year = year;
@@ -29,6 +29,49 @@ namespace MetOfficeDataAnalysis.Lib
             Sunshine = sunshine;
             CampbellStokes = campbellStokes;
             Provisional = provisional;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            var that = (MonthlyStationData)obj;
+
+            if (Year != that.Year || Month != that.Month || MaxTemperature != that.MaxTemperature
+                || MinTemperature != that.MinTemperature || AirFrost != that.AirFrost || Rain != that.Rain
+                || Sunshine != that.Sunshine || CampbellStokes != that.CampbellStokes
+                || Provisional != that.Provisional)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 17;
+            int prime = 31;
+
+            hashCode += prime * Year.GetHashCode();
+            hashCode += prime * Month.GetHashCode();
+            hashCode += prime * MaxTemperature.GetHashCode();
+            hashCode += prime * MinTemperature.GetHashCode();
+            hashCode += prime * AirFrost.GetHashCode();
+            hashCode += prime * Rain.GetHashCode();
+            hashCode += prime * Sunshine.GetHashCode();
+            hashCode += prime * CampbellStokes.GetHashCode();
+            hashCode += prime * Provisional.GetHashCode();
+
+            return hashCode;
         }
     }
 }
--- MetOfficeDataAnalysis.Lib/StationDataFile.cs
+++ MetOfficeDataAnalysis.Lib/StationDataFile.cs
@@ -13,7 +13,7 @@ namespace MetOfficeDataAnalysis.Lib
 
         public StationDataFile(TextReader reader)
         {
-            StationName = reader.ReadLine();
+            StationName = reader.ReadLine().Trim();
 
             MonthlyData = new LinkedList<MonthlyStationData>();
 
@@ -35,18 +35,107 @@ namespace MetOfficeDataAnalysis.Lib
             {
                 var line = reader.ReadLine();
 
-                MonthlyData.Add(ParseDataLine(line));
-
                 if (line == null)
                 {
                     break;
                 }
+
+                MonthlyData.Add(ParseDataLine(line));
             }
         }
 
         public static MonthlyStationData ParseDataLine(string line)
         {
+            if (String.IsNullOrWhiteSpace(line))
+            {
+                throw new ArgumentException();
+            }
+
+            char[] delimiters = { ' ' };
+
+            var parts = line.Trim().Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            for (var i = 0; i < parts.Length; i++)
+            {
+                parts[i] = parts[i].Trim();
+            }
+
+            var yearPart = parts[0];
+            var monthPart = parts[1];
+            var maxTemperaturePart = parts[2];
+            var minTemperaturePart = parts[3];
+            var airFrostPart = parts[4];
+            var rainPart = parts[5];
+            var sunshinePart = parts[6];
+
+            var provisionalPart = "";
+            if (parts.Length > 7)
+            {
+                provisionalPart = parts[7];
+            }
+
+            int year, month;
+            if (int.TryParse(yearPart, out year) && int.TryParse(monthPart, out month))
+            {
+                double? maxTemperature = PartToNullableDouble(maxTemperaturePart);
+                double? minTemperature = PartToNullableDouble(minTemperaturePart);
+
+                int? airFrost = null;
+                if (!DatumIsMissing(airFrostPart))
+                {
+                    int airFrostValue;
+                    if (int.TryParse(airFrostPart, out airFrostValue))
+                    {
+                        airFrost = airFrostValue;
+                    }
+                }
+
+                double? rain = PartToNullableDouble(rainPart);
+
+                double? sunshine = null;
+                bool? campbellStokes = null;
+                if (!DatumIsMissing(sunshinePart))
+                {
+                    if (sunshinePart.EndsWith("#"))
+                    {
+                        campbellStokes = false;
+                        sunshinePart = sunshinePart.Remove(sunshinePart.IndexOf('#'));
+                    }
+                    else
+                    {
+                        campbellStokes = true;
+                    }
+
+                    sunshine = PartToNullableDouble(sunshinePart);
+                }
+
+                var provisional = provisionalPart == "Provisional";
+
+                return new MonthlyStationData(year, month, maxTemperature, minTemperature,
+                    airFrost, rain, sunshine, campbellStokes, provisional);
+            }
+
             throw new ArgumentException("Unable to parse line!");
+        }
+
+        private static bool DatumIsMissing(string datum)
+        {
+            return datum == "---";
+        }
+
+        private static double? PartToNullableDouble(string part)
+        {
+            double? datum = null;
+            if (!DatumIsMissing(part))
+            {
+                double value;
+                if (double.TryParse(part, out value))
+                {
+                    datum = value;
+                }
+            }
+
+            return datum;
         }
     }
 }
