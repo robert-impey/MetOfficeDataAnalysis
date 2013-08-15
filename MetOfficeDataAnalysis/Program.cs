9fbfacd53cc1caaf460a4668e7bc76e506026aae
Robert Impey <robert@impey.info>
2013-08-15 17:38:51 +0100

    Able to find the hottest month from a station file.

--- MetOfficeDataAnalysis/Program.cs
+++ MetOfficeDataAnalysis/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MetOfficeDataAnalysis.Lib;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -9,6 +11,19 @@ namespace MetOfficeDataAnalysis
     {
         static void Main(string[] args)
         {
+            if (args.Length == 1)
+            {
+                var stationDataFile = new StationDataFile(new StreamReader(args[0]));
+
+                Console.WriteLine(String.Format("Station name: {0}", stationDataFile.StationName));
+                var hottestMonth = stationDataFile.MonthlyData.HottestMonth;
+                Console.WriteLine(String.Format("Hottest Month: {0}-{1}, Temp: {2} C", 
+                    hottestMonth.Year, hottestMonth.Month, hottestMonth.MaxTemperature));
+            }
+            else
+            {
+                Console.WriteLine("Not sure what to do!");
+            }
         }
     }
 }
