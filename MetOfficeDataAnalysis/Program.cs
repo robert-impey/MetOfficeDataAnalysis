64907351b4df1f00e7728b31dfb180635230cfee
Robert Impey <robert@impey.info>
2013-10-10 22:24:33 +0100

    Moved code for printing one file's data to a procedure.

--- MetOfficeDataAnalysis/Program.cs
+++ MetOfficeDataAnalysis/Program.cs
@@ -13,34 +13,39 @@ namespace MetOfficeDataAnalysis
         {
             if (args.Length == 1)
             {
-                var stationDataFile = new StationDataFile(new StreamReader(args[0]));
-
-                Console.WriteLine(String.Format("Station name: {0}", stationDataFile.StationName));
-
-                var hottestMonth = stationDataFile.MonthlyData.HottestMonth;
-                PrintMonthTemperature("Hottest month", hottestMonth.Year, hottestMonth.Month, hottestMonth.MaxTemperature);
-                var monthWithColdestMaxTemperature = stationDataFile.MonthlyData.MonthWithColdestMaxTemperature;
-                PrintMonthTemperature("Month with coldest max temperature",
-                    monthWithColdestMaxTemperature.Year, monthWithColdestMaxTemperature.Month,
-                    monthWithColdestMaxTemperature.MaxTemperature);
-
-                var coldestMonth = stationDataFile.MonthlyData.ColdestMonth;
-                PrintMonthTemperature("Coldest month", coldestMonth.Year, coldestMonth.Month, coldestMonth.MinTemperature);
-                var monthWithHottestMinTemperature = stationDataFile.MonthlyData.MonthWithHottestMinTemperature;
-                PrintMonthTemperature("Month with hottest min temperature",
-                    monthWithHottestMinTemperature.Year, monthWithHottestMinTemperature.Month,
-                    monthWithHottestMinTemperature.MinTemperature);
-
-                Console.WriteLine("Mean monthly max temperatures");
-                var meanMonthlyMaxTemperatures = stationDataFile.MonthlyData.MeanMaxTemperatures;
-                foreach (var month in meanMonthlyMaxTemperatures.Keys)
-                {
-                    Console.WriteLine(String.Format("{0} - {1:#.00} C", month, meanMonthlyMaxTemperatures[month]));
-                }
+                PrintStationFileData(args[0]);
             }
             else
             {
                 Console.WriteLine("Not sure what to do!");
+            }
+        }
+
+        private static void PrintStationFileData(string fileName)
+        {
+            var stationDataFile = new StationDataFile(new StreamReader(fileName));
+
+            Console.WriteLine(String.Format("Station name: {0}", stationDataFile.StationName));
+
+            var hottestMonth = stationDataFile.MonthlyData.HottestMonth;
+            PrintMonthTemperature("Hottest month", hottestMonth.Year, hottestMonth.Month, hottestMonth.MaxTemperature);
+            var monthWithColdestMaxTemperature = stationDataFile.MonthlyData.MonthWithColdestMaxTemperature;
+            PrintMonthTemperature("Month with coldest max temperature",
+                monthWithColdestMaxTemperature.Year, monthWithColdestMaxTemperature.Month,
+                monthWithColdestMaxTemperature.MaxTemperature);
+
+            var coldestMonth = stationDataFile.MonthlyData.ColdestMonth;
+            PrintMonthTemperature("Coldest month", coldestMonth.Year, coldestMonth.Month, coldestMonth.MinTemperature);
+            var monthWithHottestMinTemperature = stationDataFile.MonthlyData.MonthWithHottestMinTemperature;
+            PrintMonthTemperature("Month with hottest min temperature",
+                monthWithHottestMinTemperature.Year, monthWithHottestMinTemperature.Month,
+                monthWithHottestMinTemperature.MinTemperature);
+
+            Console.WriteLine("Mean monthly max temperatures");
+            var meanMonthlyMaxTemperatures = stationDataFile.MonthlyData.MeanMaxTemperatures;
+            foreach (var month in meanMonthlyMaxTemperatures.Keys)
+            {
+                Console.WriteLine(String.Format("{0} - {1:#.00} C", month, meanMonthlyMaxTemperatures[month]));
             }
         }
 
